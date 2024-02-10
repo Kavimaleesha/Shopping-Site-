3f8f724b96711eb7ccaa19e924f5735e74736fde
Kavindu Maleesha <kaviigunawardhana2000@gmail.com>
2024-02-10 22:56:57 +0530

    db connection
    db connection

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    session_start();
+    $db_name = "shopping";
+    $connection = mysqli_connect("localhost","root","",$db_name);
+
+    if(isset($_POST["add"])){
+        if(isset($_SESSION["shopping_cart"])){
+            $item_array_id = array_column($_SESSION["shopping_cart"],"product_id");
+            if(!in_array($_GET["id"],$item_array_id)){
+                $count = count($_SESSION["shopping_cart"]);
+                $item_array = array(
+                    'product_id' => $_GET["id"],
+                    'product_name' => $_POST["hidden_name"],
+                    'product_price' => $_POST["hidden_price"],
+                    'product_quantity' => $_POST["quantity"],
+                );
+                $_SESSION["shopping_cart"][$count] = $item_array;
+                echo '<script>window.location="index.php"</script>';
+            }else{
+                echo '<script>alert("Product is already in  the cart")</script>';
+                echo '<script>window.location="index.php"</script>';
+            }
+        }else{
+            $item_array = array(
+                'product_id' => $_GET["id"],
+                'product_name' => $_POST["hidden_name"],
+                'product_price' => $_POST["hidden_price"],
+                'product_quantity' => $_POST["quantity"],
+            );
+            $_SESSION["shopping_cart"][0] = $item_array;
+        }
+    }
